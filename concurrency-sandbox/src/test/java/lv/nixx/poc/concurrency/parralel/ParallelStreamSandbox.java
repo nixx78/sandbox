e39c39b585cd3ab361eea6fcb386f5117d6695b0
nixx78 <nixx78@gmail.com>
2024-09-05 09:17:26 +0300

    Sample for parallel stream with custom pool

---
+++ concurrency-sandbox/src/test/java/lv/nixx/poc/concurrency/parralel/ParallelStreamSandbox.java
@@ -0,0 +1,43 @@
+package lv.nixx.poc.concurrency.parralel;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+
+class ParallelStreamSandbox {
+
+
+    @Test
+    void parallelUsingStandartForkJoinPool() {
+
+        System.out.println("Available CPU count: " + Runtime.getRuntime().availableProcessors());
+        System.out.println("ForkJoinPool.commonPool() thread count: " + ForkJoinPool.commonPool().getParallelism());
+
+        IntStream.range(0, 100).parallel().forEach(number -> {
+            System.out.println("Thread: " + Thread.currentThread().getName() + ", number: " + number);
+        });
+
+    }
+
+    @Test
+    void parallelUsingCustomForkJoinPool() {
+        System.out.println("Available CPU count: " + Runtime.getRuntime().availableProcessors());
+
+        Collection<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        ForkJoinPool customThreadPool = new ForkJoinPool(2);
+
+        Runnable runnable = () ->
+                numbers.parallelStream().forEach(number -> {
+                    System.out.println("Thread: " + Thread.currentThread().getName() + ", number: " + number);
+                });
+
+        customThreadPool.submit(runnable).join();
+
+        customThreadPool.shutdown();
+    }
+
+}

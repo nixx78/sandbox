7f47c32ab8aec9d4810733c0bda8ffcac7ed7fb3
nixx78 <nixx78@gmail.com>
2024-11-06 16:21:11 +0200

    Trivial sample with BigDecimal sorting added

--- collection-lambda-sandbox/src/test/java/lv/nixx/poc/collection/compare/ComparatorSandbox.java
+++ collection-lambda-sandbox/src/test/java/lv/nixx/poc/collection/compare/ComparatorSandbox.java
@@ -1,20 +1,26 @@
 package lv.nixx.poc.collection.compare;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lv.nixx.poc.collection.domain.Transaction;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ComparatorSandbox {
+
+class ComparatorSandbox {
 
     private static final Comparator<BigDecimal> nullSafeBigDecimalComparator = Comparator.nullsFirst(BigDecimal::compareTo);
     private static final Comparator<String> nullSafeStringComparator = Comparator.nullsFirst(String::compareTo);
 
     @Test
-    public void compareTest() {
+    void compareTest() {
 
         assertEquals(0, nullSafeBigDecimalComparator.compare(null, null));
         assertEquals(0, nullSafeBigDecimalComparator.compare(BigDecimal.valueOf(10.00), BigDecimal.valueOf(10.00)));
@@ -24,7 +30,7 @@ public class ComparatorSandbox {
     }
 
     @Test
-    public void compareTxn() {
+    void compareTxn() {
         Transaction t1 = new Transaction("id1", 10.10, "ACC1", null);
         Transaction t2 = new Transaction("id1", 10.10, "ACC1", null);
 
@@ -35,6 +41,28 @@ public class ComparatorSandbox {
                 .thenComparing(Transaction::getCurrency, nullSafeStringComparator);
 
         assertEquals(0, c.compare(t1, t2));
+    }
+
+    @Test
+    void sortBigDecimalNullsFirst() {
+
+        List<BigDecimal> sortedList = Stream.of(
+                        new Data(BigDecimal.valueOf(10)),
+                        new Data(BigDecimal.valueOf(1)),
+                        new Data(BigDecimal.valueOf(100)),
+                        new Data(null)
+                )
+                .sorted(Comparator.comparing(Data::getValue, Comparator.nullsFirst(Comparator.naturalOrder())))
+                .map(Data::getValue)
+                .toList();
+
+        assertThat(sortedList).containsExactly(null, BigDecimal.valueOf(1), BigDecimal.valueOf(10), BigDecimal.valueOf(100));
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class Data {
+        BigDecimal value;
     }
 
 }
